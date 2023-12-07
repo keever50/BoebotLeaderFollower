d54c00ef236f5d55f1fac9d3d15a05dc39bdaf40
Kevin Witteveen <kevinwit1999@gmail.com>
2023-12-07 20:56:30 +0100

    Test main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,60 @@
+// #include <Arduino.h>
+// #include <rf_receive2.h>
+
+// void setup() {
+//   Serial.begin(115200);
+//   rf_receive_init();
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+//   if(rf_receive_get_data_ready(1))
+//   {
+//     for(int i=0;i<8;i++)
+//     {
+//       float bit = rf_receive_get_data(i);
+//       if(bit<0.5)
+//       {
+//         Serial.print("1");
+//       }else{
+//         Serial.print("0");
+//       }
+      
+//     }
+//     Serial.println("");
+//     for(int i=0;i<8;i++)
+//     {
+//       float bit = rf_receive_get_data(i);
+//       Serial.print(bit);
+//       Serial.print(" ");
+      
+//     }
+//     Serial.println("");
+
+//   }
+//   delay(1);
+// }
+
+
 #include <Arduino.h>
-#include <rf_receive.h>
+#include <rf_transmit.h>
 
-void setup() {
-  Serial.begin(9600);
-  rf_receive_init();
+void setup()
+{
+  pinMode(trans_pin, OUTPUT);
 }
+void loop()
+{
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  if(rf_receive_data_ready(1))
+  char bits[] = {1,1,0,0,1,0,1,0};
+  for(int i=0; i<10;i++)
   {
-    for(int i = 0; i<10; i++)
-    {
-      Serial.println(rf_receive_data_buffer(i));
-    }
-    Serial.println("STOP");
+    transmit_binary(bits, sizeof(bits)); 
+    delay(10); 
   }
-  delay(500);
-  Serial.println("Listening");
 
 
+  delay(20000);
 }
