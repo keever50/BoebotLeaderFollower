d797ed8cd5cbf054adb902159bcf2ba184f13484
Kevin Witteveen <kevinwit1999@gmail.com>
2023-12-07 20:05:46 +0100

    RF update

--- lib/RF/rf_receive2.h
+++ lib/RF/rf_receive2.h
@@ -3,9 +3,13 @@
 
 #define RF_RECEIVE_PIN  2
 
-
+#define RF_RECEIVE_FREQ_HI  490
+#define RF_RECEIVE_FREQ_LOW 470
 
 void rf_receive_init();
 void rf_debug(float *freq, float *duty, int *detect);
+
+char rf_receive_get_data_ready( char reset );
+float rf_receive_get_data( int bit );
 
 #endif
--- lib/RF/rf_recieve2.cpp
+++ lib/RF/rf_recieve2.cpp
@@ -5,6 +5,24 @@ volatile float rf_debug_frequency;
 volatile float rf_debug_duty;
 volatile int rf_detection;
 
+volatile char rf_receive_data_ready;
+volatile float rf_receive_bitbuffer[8]={0};
+
+char rf_receive_get_data_ready( char reset )
+{
+    char flag = rf_receive_data_ready;
+    if(reset)
+    {
+        rf_receive_data_ready=0;
+    }
+    return flag;
+}
+
+float rf_receive_get_data( int bit )
+{
+    return rf_receive_bitbuffer[bit];
+}
+
 void rf_debug(float *freq, float *duty, int *detect)
 {
     *freq=rf_debug_frequency;
@@ -23,6 +41,7 @@ void rf_receive_ISR()
     static float end_time=0;
     static float wave_period;
     static float wave_frequency;
+    static int bit_count=0;
 
     int state = digitalRead(RF_RECEIVE_PIN);
     if(state==0)
@@ -39,12 +58,38 @@ void rf_receive_ISR()
         //debug
         rf_debug_frequency=wave_frequency;
         rf_debug_duty=duty;
-        if(wave_frequency>900 && wave_frequency < 1000)
+
+        //If no data has been received before
+        if(rf_receive_data_ready==0)
         {
-            digitalWrite(LED_BUILTIN, HIGH);
-        }else{
-            digitalWrite(LED_BUILTIN, LOW);
+            //Target frequency
+            if(wave_frequency>RF_RECEIVE_FREQ_LOW && wave_frequency < RF_RECEIVE_FREQ_HI)
+            {
+                digitalWrite(LED_BUILTIN, HIGH);
+
+                //Target duty
+                if(duty > 0.60 || duty < 0.40)
+                {
+                    if(bit_count<8)
+                    {
+                        bit_count=bit_count+1;
+                        rf_receive_bitbuffer[bit_count]=duty;
+                    }
+                }
+
+            }else{
+                digitalWrite(LED_BUILTIN, LOW);
+                if(bit_count>0)
+                {
+                    rf_receive_data_ready=1;
+                    bit_count=0;
+                }
+                
+                
+            }
         }
+
+
 
 
     }else{
