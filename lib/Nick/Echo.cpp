3f8a9825380d6da01cc755e5e4a7f39bd43820ee
Nick Piek <Nickpiek2004@gmail.com>
2024-01-08 11:29:04 +0100

    test

--- lib/Nick/Echo.cpp
+++ lib/Nick/Echo.cpp
@@ -1,24 +1,30 @@
-#include <Echo.h>
 #include <Arduino.h>
+#include <Echo.h>
 
-void echo_innit()
+void setup()
 {
     Serial.begin(9600);
-    pinMode(ECHO_ECHOPIN, OUTPUT);
-    pinMode(ECHO_TRIGGERPIN, INPUT);
+    pinMode(ECHO_ECHOPIN, INPUT);
+    pinMode(ECHO_TRIGGERPIN, OUTPUT);
+}
+
+void loop()
+{
+  echo_detect();
 }
 
 float echo_detect()
 {
     digitalWrite(ECHO_TRIGGERPIN, LOW);
     delayMicroseconds(2);
-    digitalWrite(ECHO_ECHOPIN, HIGH);
+    digitalWrite(ECHO_TRIGGERPIN, HIGH);
     delayMicroseconds(10);
     digitalWrite(ECHO_TRIGGERPIN, LOW);
     
     duration= pulseIn(ECHO_ECHOPIN, HIGH);
-    distance= (duration * .0343)/2;
-    Serial.print("Afstand: ");
+    distance= (duration * 0.0343)/2;
+    Serial.print("Afstand: \n");
     Serial.print(distance);
+    Serial.println();
     delay(100);
 }
