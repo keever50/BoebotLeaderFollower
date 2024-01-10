6888dac8abc26ce34e3d6e2cd870449c941ba5c6
Xander <xanderperry@hotmail.nl>
2024-01-10 15:45:20 +0100

    esfrgd

--- src/oudmain.cpp
+++ src/oudmain.cpp
@@ -1,43 +1,43 @@
-#include <Arduino.h>
-#include <LCG.h>
-#include <TimerOne.h>
+// #include <Arduino.h>
+// #include <LCG.h>
+// #include <TimerOne.h>
 
-void pulse(int pin, int freq, int duty);
+// void pulse(int pin, int freq, int duty);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Timer1.initialize(26);
-  Timer1.pwm(9, 512);
-}
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
+//   Timer1.initialize(26);
+//   Timer1.pwm(9, 512);
+// }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// void loop() {
+//   // put your main code here, to run repeatedly:
   
-}
+// }
 
 
 
 
-/*
-void setup() 
-{
-  Serial.begin(9600);
+// /*
+// void setup() 
+// {
+//   Serial.begin(9600);
   
-  EEPROM.put(0, 233);
-}
+//   EEPROM.put(0, 233);
+// }
 
-void loop() 
-{
-  // put your main code here, to run repeatedly:
-  Serial.print("New seed: ");
-  update_seed();
-  for(int i = 0; i < 50; i++)
-  {
-    Serial.print(LCG());
-    Serial.print("\t");
-  }
-  Serial.println();
-  delay(1000);
-}
-*/
+// void loop() 
+// {
+//   // put your main code here, to run repeatedly:
+//   Serial.print("New seed: ");
+//   update_seed();
+//   for(int i = 0; i < 50; i++)
+//   {
+//     Serial.print(LCG());
+//     Serial.print("\t");
+//   }
+//   Serial.println();
+//   delay(1000);
+// }
+// */
