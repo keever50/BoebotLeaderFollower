85be6985c658623c13c3947a73ff7857f433c5be
Xander <xanderperry@hotmail.nl>
2024-01-10 12:16:43 +0100

    Refactor code to improve performance and readability

---
+++ src/oudmain.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <LCG.h>
+#include <TimerOne.h>
+
+void pulse(int pin, int freq, int duty);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Timer1.initialize(26);
+  Timer1.pwm(9, 512);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+
+
+
+/*
+void setup() 
+{
+  Serial.begin(9600);
+  
+  EEPROM.put(0, 233);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  Serial.print("New seed: ");
+  update_seed();
+  for(int i = 0; i < 50; i++)
+  {
+    Serial.print(LCG());
+    Serial.print("\t");
+  }
+  Serial.println();
+  delay(1000);
+}
+*/
