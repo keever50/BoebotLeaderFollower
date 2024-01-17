b043cd92b6499392fbb2e26709b2d586a2add388
Kevin Witteveen <kevinwit1999@gmail.com>
2024-01-17 14:20:46 +0100

    Beacon seeking

--- src/follower_main.cpp
+++ src/follower_main.cpp
@@ -21,14 +21,56 @@ void setup() {
   pwmgen1_set_duty_B(0.075+0.0);
   
   seeker_init();
-
+  seeker_init_auto();
 }
 
 int t;
+float beacon_angle=180;
 void loop() {
-  //delayMicroseconds(1);
-  int a = seeker_stepdetect();
-  Serial.println(a);
+  delay(100);
+  //int a = seeker_stepdetect();
+  //Serial.println(a);
+
+  //Serial.println(seeker_get_step());
+
+
+  if(seeker_did_full_revolution(1)==1)
+  {
+    float avg=0;
+    float count=0;
+    for(int i=0;i<SEEKER_STEPS_PER_REVOLUTION;i++)
+    {
+      char det = seeker_get_detections(i);
+      if(det)
+      {
+        Serial.print("Detection on ");
+        Serial.println(i);
+        avg=avg+i;
+        count++;
+      }
+    }
+    avg=avg/count;
+    Serial.print("Average: ");
+    Serial.println(avg);
+
+    float ratio = avg/(SEEKER_STEPS_PER_REVOLUTION);
+    Serial.print("Deg: ");
+    beacon_angle=ratio*360;
+    Serial.println(beacon_angle);
+    
+
+    /*Steering*/
+    const float max_steering=0.05;
+    float steer = (beacon_angle-180)/(180*8);
+    steer=steer*max_steering;
+    if(steer>max_steering){steer=max_steering;}
+    if(steer<-max_steering){steer=-max_steering;}
+    
+    pwmgen1_set_duty_A(0.075+steer);
+  }
+
+
+
 
 }
 
--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,10 +1,15 @@
 #ifdef TESTING
 
 #include <Arduino.h>
+#include <pwmgen1.h>
 
 void setup()
 {
-
+    pwmgen1_clock_mode( 2 );
+    pwmgen1_start();
+    pwmgen1_set_frequency(38500);
+    pwmgen1_set_duty_A(0.5);
+    
 }
 
 void loop()
