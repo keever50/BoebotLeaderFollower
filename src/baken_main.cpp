baf85ed47a7d38b6700308091b998ecb6f482868
Nick Piek <Nickpiek2004@gmail.com>
2024-01-22 13:58:19 +0100

    test

--- src/baken_main.cpp
+++ src/baken_main.cpp
@@ -1,20 +1,24 @@
 #ifdef BAKEN
 #include <Arduino.h>
+#include <rf_receive2.h>
+#define IRpin 9
 
 void setup()
 {
-Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  noInterrupts();
+  Serial.begin(9600);
+  rf_receive_init;
+  pinMode(IRpin, OUTPUT);
   pinMode(13, OUTPUT);
-  TCCR1A = 0;     // set entire TCCR1A register to 0
-  TCCR1B = 0;     // same for TCCR1B
+  TCCR1A = 0;     
+  TCCR1B = 0;     
   TCCR1A |= (1 << COM1A0);
-  TCCR1B |= (1 << CS11) | (1<< WGM12);  // 8 prescaler
+  TCCR1B |= (1 << CS11) | (1<< WGM12);  
   TIMSK1 |= (1 << OCIE1A);
   OCR1A = (F_CPU/16)/38500;
 }
 
-ISR(TIMER1_COMPA_vect)//timer1 interrupt 1Hz toggles pin 13 (LED
+ISR(TIMER1_COMPA_vect)
 {
     static int t;
     digitalWrite(13, t);
@@ -23,7 +27,15 @@ ISR(TIMER1_COMPA_vect)//timer1 interrupt 1Hz toggles pin 13 (LED
 
 void loop()
 {
-    delay(500);
+  if(rf_recieve_char_data()==60);
+  {
+    interrupts();
+  }
+
+  else
+  {
+    
+  }
 }
 
 #endif
