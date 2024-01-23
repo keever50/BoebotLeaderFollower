501094c7d52f15972bd058e83959f90ea9b19cce
Nick Piek <Nickpiek2004@gmail.com>
2024-01-23 14:38:52 +0100

    R

--- src/baken_main.cpp
+++ src/baken_main.cpp
@@ -1,51 +1,82 @@
 #ifdef BAKEN
 #include <Arduino.h>
 #include <rf_receive2.h>
+#define BLAUW_pin 3
+#define GROEN_pin 4
+#define ROOD_pin 5
+#define Servo_pin 7
 #define IRpin 9
+void LED_uit();
+void LED_aan();
 char i;
-char flag = 60;
+char aan = 0;
 
 void setup()
 {
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-
+  pinMode(BLAUW_pin, OUTPUT);
+  pinMode(GROEN_pin, OUTPUT);
+  pinMode(ROOD_pin, OUTPUT);
+  pinMode(Servo_pin, OUTPUT);
   noInterrupts();
   Serial.begin(9600);
   rf_receive_init();
   pinMode(IRpin, OUTPUT);
   pinMode(13, OUTPUT);
   TCCR1A = 0;     
-  TCCR1B = 0;     
-  TCCR1A |= (1 << COM1A0);
+  TCCR1B = 0;
+  TIMSK1 = 0;    
   TCCR1B |= (1 << CS11) | (1<< WGM12);  
-  TIMSK1 |= (1 << OCIE1A);
   OCR1A = (F_CPU/16)/38500;
   interrupts();
+  LED_uit();
 }
 
 ISR(TIMER1_COMPA_vect)
 {
     static int t;
-    digitalWrite(13, t);
+    digitalWrite(IRpin, t);
     t=!t;
 }
 
+int toggle_IR=1;
 void loop()
 {
-  if(flag==60)
+  if (toggle_IR==0)
   {
-    i=5;
-    digitalWrite(i, HIGH);
-    delay(100);
-    digitalWrite(i, LOW);
-    delay(100);
+    LED_aan();
   }
 
-  else
+  if (toggle_IR==1)
   {
-    for(i=3; i<=4; i++)
+    LED_uit();
+  }
+  switch(rf_receive_char_data())
+  {
+  case 60:
+  {
+    if(toggle_IR)
+    {
+      TIMSK1 |= (1 << OCIE1A);
+      toggle_IR=0;
+    }
+    else{
+      TIMSK1 = 0;
+      toggle_IR=1;
+    }
+
+    break;
+  }
+  default:
+  {
+    break;
+  }
+   break;
+  }
+  }
+
+void LED_uit()
+{
+  for(i=3; i<=4; i++)
     {
       digitalWrite(i, HIGH);
       delay(200);
@@ -55,8 +86,20 @@ void loop()
       digitalWrite(i, HIGH);
       delay(200);
     }
-  }
-   
+}
+
+void LED_aan()
+{
+   digitalWrite(BLAUW_pin, LOW);
+    digitalWrite(GROEN_pin, LOW);
+    digitalWrite(ROOD_pin, HIGH);
+    delay(100);
+    digitalWrite(ROOD_pin, LOW);
+    delay(100);
+    digitalWrite(Servo_pin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(Servo_pin, LOW);
+    delay(10);
 }
 
 #endif
